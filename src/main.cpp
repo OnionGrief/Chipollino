0ac390b29f145b699c832d56ef0fa4407d4f93d2
StarikTenger <StarikTenger@gmail.com>
2022-10-11 19:03:45 +0300

    regex example (#15)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <Regex.h>
+#include "Example.h"
 using namespace std;
 
 int main() {
 	cout << "Chipollino :-)\n";
-	string reg = "((((a*c)))|(bd|q))";
-	Regex r(reg);
-	r.pre_order_travers();
-	r.clear();
+	Example::regex_parsing();
 }
