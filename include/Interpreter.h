64015572d64d44c0e9fb0559d4a0c80f86b9c8ae
StarikTenger <StarikTenger@gmail.com>
2022-10-18 01:27:29 +0300

    lexer (#8)

--- include/Interpreter.h
+++ include/Interpreter.h
@@ -2,100 +2,123 @@
 #include <fstream>
 #include <string>
 #include <set>
+#include <deque>
 #include "Regex.h"
-using namespace std;
 
 class Interpreter {
+public:
+	
+	Interpreter() {
+		Lexer s;
+		auto lexems = s.parse_string("MN1 = Glushkov ((ab)*|a)* !!");
+	}
 private:
-	struct Lexem {
-		enum Type {
-			error,
-			equalSign,
-			doubleExclamation,
-			function,
-			id,
-			regex,
-			number,
-			predicate,
-			test
+	
+	class Lexer {
+	public:
+		struct Lexem {
+			enum Type {
+				error,
+				equalSign,
+				doubleExclamation,
+				function,
+				id,
+				regex,
+				number,
+				predicate,
+				test
+			};
+
+			Type type = error;
+			// Если type = id | function | predicate
+			string value = "";
+			// Усли type = number
+			int num = 0;
+
+			Lexem(Type type = error, string value = "");
+			Lexem(int num);
 		};
 
-		Type type = error;
-		string value = "";
-		int num = 0;
+		Lexer();
+		void load_file(string path);
+		// Возвращает лексемы, разбитые по строчкам
+		vector<vector<Lexem>> get_lexems();
+		// Бьёт строку на лексемы (без перевода строки)
+		vector<Lexem> parse_string(string);
+	private:
 
-		Lexem(Type type = error, string value = "");
-		Lexem(int num);
+		// Здесь храним строку и место, откуда её читаем
+		struct {
+		public:
+			string str = "";
+			int pos = 0;
+
+			void save() {
+				saves.push_back(pos);
+			}
+			void restore() {
+				pos = saves.back();
+				saves.pop_back();
+			}
+		private:
+			deque<int> saves;
+
+		} input;
+
+		bool eof();
+		char current_symbol();
+		void next_symbol();
+		void skip_spaces();
+		bool scan_word(string);
+		string scan_until_space();
+
+		Lexem scan_equalSign();
+		Lexem scan_doubleExclamation();
+		Lexem scan_function();
+		Lexem scan_id();
+		Lexem scan_regex();
+		Lexem scan_number();
+		Lexem scan_predicate();
+		Lexem scan_test();
+		Lexem scan_lexem();
+
+		// TODO: сделать класс хранения функции с входным и выходным типами
+		// где-то надо хранить map с названиями вместо этого set
+		set<string> functions = {
+			"Thompson",
+			"IlieYu",
+			"Antimirov",
+			"Arden",
+			"Glushkov",
+			"Determinize",
+			"RemEps",
+			"Linearize",
+			"Minimize",
+			"Reverse",
+			"Annote",
+			"DeLinearize",
+			"Complement",
+			"MergeBisim",
+			"PumpLength",
+			"ClassLength",
+			"KSubSet",
+			"Normalize",
+			"States",
+			"ClassCard",
+			"Ambiguity",
+			"Width",
+			"MyhillNerode",
+			"Simplify",
+		};
+
+		set<string> predicates = {
+			"Bisimilar",
+			"Minimal",
+			"Subset",
+			"Equiv",
+			"Minimal",
+			"Equal",
+			"SemDet",
+		};
 	};
-
-	// Здесь храним строку и место, откуда её читаем
-	struct {
-		string str = "";
-		int pos = 0;
-	} input;
-
-	bool eof();
-	char current_symbol();
-	void next_symbol();
-	void skip_spaces();
-	bool scan_word(string);
-	string scan_until_space();
-
-	Lexem scan_equalSign();
-	Lexem scan_doubleExclamation();
-	Lexem scan_function();
-	Lexem scan_object();
-	Lexem scan_id_lvalue(); // объявление нового ID
-	Lexem scan_id_rvalue(); // считывание известного ID
-	Lexem scan_regex();
-	Lexem scan_number();
-	Lexem scan_predicate();
-	Lexem scan_test();
-
-	// TODO: сделать класс хранения функции с входным и выходным типами
-	// где-то надо хранить map с названиями вместо этого set
-	set<string> functions = {
-		"Thompson",
-		"IlieYu",
-		"Antimirov",
-		"Arden",
-		"Glushkov",
-		"Determinize",
-		"RemEps",
-		"Linearize",
-		"Minimize",
-		"Reverse",
-		"Annote",
-		"DeLinearize",
-		"Complement",
-		"MergeBisim",
-		"PumpLength",
-		"ClassLength",
-		"KSubSet",
-		"Normalize",
-		"States",
-		"ClassCard",
-		"Ambiguity",
-		"Width",
-		"MyhillNerode",
-		"Simplify",
-	};
-
-	set<string> predicates = {
-		"Bisimilar",
-		"Minimal",
-		"Subset",
-		"Equiv",
-		"Minimal",
-		"Equal",
-		"SemDet",
-	};
-
-	set<string> ids = {};
-
-public:
-	Interpreter();
-	void load_file(string path);
-	void run_operation();
-	void run_all();
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include "Regex.h"
+#include "Interpreter.h"
 using namespace std;
 
 int main() {
-	cout << "Chipollino :-)\n";
-	string reg = "((((a*c)))|(bd|q))";
-	Regex r(reg);
-	r.pre_order_travers();
-	r.clear();
+	Interpreter();
 }
