6101469202545bd9d8a1591a6a145cf4dc74ac3e
mea250802 <mea250802@gmail.com>
2022-10-25 13:53:20 +0300

    some return errors fixed

--- include/objects/Regex.h
+++ include/objects/Regex.h
@@ -69,10 +69,10 @@ class Regex : BaseObject {
 	// Производная по символу
 	bool derevative_with_respect_to_sym(Regex* respected_sym,
 										const Regex* reg_e,
-										Regex* result) const;
+										Regex& result) const;
 	// Производная по префиксу
 	bool derevative_with_respect_to_str(std::string str, const Regex* reg_e,
-										Regex* result) const;
+										Regex& result) const;
 	FiniteAutomaton get_tompson(int);
 
 	vector<Lexem>* first_state(); // начальные состояния для to_glushkov
