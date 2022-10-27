0289510e54b84f98f0dbac23273d5bf7049f410e
Teriukha Mikhail <76103754+xtoter@users.noreply.github.com>
2022-10-27 23:49:09 +0300

    Update src/objects/Regex.cpp (№46)
    Андрей хитрый (ссылка на анекдот про хитрый)
    
    Co-authored-by: Andrei Ilin <48261272+StarikTenger@users.noreply.github.com>

--- src/objects/Regex.cpp
+++ src/objects/Regex.cpp
@@ -559,7 +559,6 @@ pair<vector<State>, int> Regex::get_tompson(int max_index) const {
 					// map_l[elem] = trans;
 				}
 			}
-			// cout << al->states[i].identifier << " " <<
 			// al->states[i].index
 			// <<"\n";
 			s.push_back(State(al.first[i].index, {}, al.first[i].identifier,
