dc86e8dd18ebd786d67eb0e6e12dc6cb4405fda0
Alexander Delman <adelman2112@gmail.com>
2022-10-29 16:04:40 +0300

    Правки после merge

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -149,7 +149,7 @@ void Logger::log(string a1, string a2, string a3, const FiniteAutomaton& fa1,
 
 void Logger::log(string r1, string r2, int step, vector<Tester::word> words) {
 	if (!active) return;
-	if (skip) return;
+	if (step_counter > 1) return;
 	Logger::log("Язык, основанный на регулярке", r1);
 	Logger::log("Слова порождаются регуляркой r2", r2);
 	Logger::log("Шаг итерации", to_string(step));
@@ -162,26 +162,26 @@ void Logger::log(string r1, string r2, int step, vector<Tester::word> words) {
 		s1 = s1 + " & ";
 		s1 = s1 + "Принадлежность языку";
 		s1 = s1 + "\\\\";
-		out << s1 <<endl;
+		out << s1 << endl;
 		for (int i = 0; i < words.size(); i++) {
 			string w1 = to_string(words[i].iterations_num + 1);
 			string w2 = to_string(words[i].time);
 			string w3 = to_string(words[i].is_belongs);
-			if (i != words.size()-1) {
-				out << w1 + " & " + w2 + " & " + w3 + " \\\\" <<endl;
+			if (i != words.size() - 1) {
+				out << w1 + " & " + w2 + " & " + w3 + " \\\\" << endl;
 			} else {
-				out << w1 + " & " + w2 + " & " + w3 <<endl;
+				out << w1 + " & " + w2 + " & " + w3 << endl;
 			}
-			
 		}
 		out << "\\end{tabular}\n" << endl;
 	}
-	out.close(); 
+	out.close();
 }
 
-void Logger::log(const FiniteAutomaton& fa1, string r2, int step, vector<Tester::word> words1, vector<Tester::word> words2) {
+void Logger::log(const FiniteAutomaton& fa1, string r2, int step,
+				 vector<Tester::word> words1, vector<Tester::word> words2) {
 	if (!active) return;
-	if (skip) return;
+	if (step_counter > 1) return;
 	ofstream out("./resources/report.tex", ios::app);
 	string f1 = fa1.to_txt();
 	if (out.is_open()) {
@@ -190,7 +190,10 @@ void Logger::log(const FiniteAutomaton& fa1, string r2, int step, vector<Tester:
 			AutomatonToImage::to_image(f1, i);
 			out << "Автомат\n" << endl;
 			char si[256];
-    		sprintf(si, "\\includegraphics[width=5in, keepaspectratio]{./resources/output%d.png}\n", i);
+			sprintf(si,
+					"\\includegraphics[width=5in, "
+					"keepaspectratio]{./resources/output%d.png}\n",
+					i);
 			out << si << endl;
 		}
 	}
@@ -200,39 +203,39 @@ void Logger::log(const FiniteAutomaton& fa1, string r2, int step, vector<Tester:
 	out.open("./resources/report.tex", ios::app);
 	if (out.is_open()) {
 		out << "\\begin{tabular}{lll}\n" << endl;
-		out << "\\multicolumn{3}{c}{Таблица по алгоритму с возвратами} \\\\" << endl;
+		out << "\\multicolumn{3}{c}{Таблица по алгоритму с возвратами} \\\\"
+			<< endl;
 		string s1 = "Количество итераций";
 		s1 = s1 + " & ";
 		s1 = s1 + "Время парсинга";
 		s1 = s1 + " & ";
 		s1 = s1 + "Принадлежность языку";
 		s1 = s1 + "\\\\";
-		out << s1 <<endl;
+		out << s1 << endl;
 		for (int i = 0; i < words1.size(); i++) {
 			string w1 = to_string(words1[i].iterations_num + 1);
 			string w2 = to_string(words1[i].time);
 			string w3 = to_string(words1[i].is_belongs);
-			if (i != words1.size()-1) {
-				out << w1 + " & " + w2 + " & " + w3 + " \\\\" <<endl;
+			if (i != words1.size() - 1) {
+				out << w1 + " & " + w2 + " & " + w3 + " \\\\" << endl;
 			} else {
-				out << w1 + " & " + w2 + " & " + w3 <<endl;
+				out << w1 + " & " + w2 + " & " + w3 << endl;
 			}
-			
 		}
 		out << "\\end{tabular}\n" << endl;
 		out << "\\begin{tabular}{lll}\n" << endl;
-		out << "\\multicolumn{3}{c}{Таблица по параллельному алгоритму} \\\\" << endl;
-		out << s1 <<endl;
+		out << "\\multicolumn{3}{c}{Таблица по параллельному алгоритму} \\\\"
+			<< endl;
+		out << s1 << endl;
 		for (int i = 0; i < words2.size(); i++) {
 			string w1 = to_string(words2[i].iterations_num + 1);
 			string w2 = to_string(words2[i].time);
 			string w3 = to_string(words2[i].is_belongs);
-			if (i != words2.size()-1) {
-				out << w1 + " & " + w2 + " & " + w3 + " \\\\" <<endl;
+			if (i != words2.size() - 1) {
+				out << w1 + " & " + w2 + " & " + w3 + " \\\\" << endl;
 			} else {
-				out << w1 + " & " + w2 + " & " + w3 <<endl;
+				out << w1 + " & " + w2 + " & " + w3 << endl;
 			}
-			
 		}
 		out << "\\end{tabular}\n" << endl;
 	}
