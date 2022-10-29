9c588e803efab1c821aaacc0ad24d56ef3dea736
Анна Терентьева <annyshka20072002@gmail.com>
2022-10-29 14:35:51 +0300

    парсинг по автомату (#57)

--- src/objects/Tester.cpp
+++ src/objects/Tester.cpp
@@ -4,25 +4,23 @@ void Tester::test(string lang, string r2, int step) {
 	vector<word> words;
 	Regex r;
 	r.from_string(r2);
+	FiniteAutomaton automaton = r.to_tompson();
 	Regex R;
 	R.from_string(lang);
 
 	for (int i = 0; i < 13; i++) {
 		string w = r.get_iterated_word(i * step);
-		cout << w;
-		if (w.length() > 700) return;
+		// cout << w;
 		int start = clock();
-
-		
-		bool is_belongs =
-			parsing_by_regex(lang, w); // падает на больших словах((
+		bool is_belongs = automaton.parsing_by_nfa(w);
+		// parsing_by_regex(lang, w); // падает на больших словах((
 		int end = clock();
-		int time = (end - start) / CLOCKS_PER_SEC;
-		cout << is_belongs << endl;
+		int time = (end - start); // ((clock_t)100);
+		// cout << " " << time << endl;
+		// cout << is_belongs << endl;
 		words.push_back({i * step, time, is_belongs});
 		if (time >= 180) return;
 	}
-	tableRegex table = {lang, r2, step, words};
 
 	/*for (int i = 0; i < words.size(); i++) {
 		cout << words[i].iterations_num << " " << words[i].time << " "
@@ -45,12 +43,11 @@ void Tester::test(FiniteAutomaton lang, string r2, int step) {
 		int start = clock();
 		bool is_belongs = lang.parsing_by_nfa(w);
 		int end = clock();
-		int time = (end - start) / CLOCKS_PER_SEC;
+		int time = (end - start); // CLOCKS_PER_SEC;
 		// cout << is_belongs << endl;
 		words.push_back({i * step, time, is_belongs});
 		if (time >= 180) return;
 	}
-	tableFA table = {lang, "((ab)*a)*", 2, words};
 
 	/* место для вызова логгера */
 	// my_loger(tableFA); а мб можно не так
