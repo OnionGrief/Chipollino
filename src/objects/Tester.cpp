eeb08fda8d97e4a5d5408a33dcbd40780650e85c
alina8031 <al_chibizova@mail.ru>
2022-10-29 22:30:56 +0300

    логи тестера (#1)

--- src/objects/Tester.cpp
+++ src/objects/Tester.cpp
@@ -3,7 +3,7 @@
 void Tester::test(string lang, string r2, int step) {
 	vector<word> words;
 	vector<double> times;
-	vector<bool> booleans;
+	vector<bool> belongs;
 	Regex r;
 	r.from_string(r2);
 	FiniteAutomaton automaton = r.to_tompson();
@@ -12,7 +12,7 @@ void Tester::test(string lang, string r2, int step) {
 	using clock = std::chrono::high_resolution_clock;
 
 	for (int i = 0; i < 13; i++) {
-		string w = r.get_iterated_word(i * 200);
+		string w = r.get_iterated_word(i * step);
 		// cout << w;
 
 		const auto start = clock::now();
@@ -21,24 +21,25 @@ void Tester::test(string lang, string r2, int step) {
 		const auto end = clock::now();
 		const double elapsed =
 			std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
-				.count()*1e-6;
+				.count() *
+			1e-6;
 		// int time = (int) elapsed;
 		// int time = (end - start); // ((clock_t)100);
-		//cout << " " << elapsed << endl;
+		// cout << " " << elapsed << endl;
 		// cout << is_belongs << endl;
 		times.push_back(elapsed);
-		booleans.push_back(is_belongs);
+		belongs.push_back(is_belongs);
 		if (elapsed >= 180) return;
 	}
-	/*Logger::init_step("Test");
-	Logger::log(lang, r2, step, times, booleans);
-	Logger::finish_step();*/
+	Logger::init_step("Test");
+	Logger::log(lang, r2, step, times, belongs);
+	Logger::finish_step();
 }
 
 void Tester::test(FiniteAutomaton lang, string r2, int step) {
 	vector<word> words;
 	vector<double> times;
-	vector<bool> booleans;
+	vector<bool> belongs;
 	Regex r;
 	r.from_string(r2);
 	using clock = std::chrono::high_resolution_clock;
@@ -49,15 +50,16 @@ void Tester::test(FiniteAutomaton lang, string r2, int step) {
 		const auto start = clock::now();
 		bool is_belongs = lang.parsing_by_nfa(w);
 		const auto end = clock::now();
-		
+
 		const double elapsed =
 			std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
-				.count()*1e-6;
+				.count() *
+			1e-6;
 		times.push_back(elapsed);
-		booleans.push_back(is_belongs);
+		belongs.push_back(is_belongs);
 		// int time = (end - start); // CLOCKS_PER_SEC;
-		//cout << is_belongs << " " << elapsed << endl;
-		//words.push_back({i * step, time, is_belongs});
+		// cout << is_belongs << " " << elapsed << endl;
+		// words.push_back({i * step, time, is_belongs});
 		if (elapsed >= 180) return;
 	}
 
@@ -65,7 +67,7 @@ void Tester::test(FiniteAutomaton lang, string r2, int step) {
 	// my_loger(tableFA); а мб можно не так
 	// my_loger(lang, r2, step, words); без лишней структуры
 	Logger::init_step("Test");
-	Logger::log(lang, r2, step, times, booleans);
+	Logger::log(lang, r2, step, times, belongs);
 	Logger::finish_step();
 }
 
