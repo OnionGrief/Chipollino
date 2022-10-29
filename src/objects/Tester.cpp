cf1227c69bb94d03618030a3390b0e5527a59c7b
Анна Терентьева <annyshka20072002@gmail.com>
2022-10-29 22:26:46 +0300

    исправлен счетчик времени

--- src/objects/Tester.cpp
+++ src/objects/Tester.cpp
@@ -7,32 +7,31 @@ void Tester::test(string lang, string r2, int step) {
 	Regex r;
 	r.from_string(r2);
 	FiniteAutomaton automaton = r.to_tompson();
+	// automaton.determinize();
 	Regex R;
 	R.from_string(lang);
 	using clock = std::chrono::high_resolution_clock;
 
 	for (int i = 0; i < 13; i++) {
 		string w = r.get_iterated_word(i * step);
-		cout << w;
+		// cout << w;
 
 		const auto start = clock::now();
 		bool is_belongs = automaton.parsing_by_nfa(w);
 		// parsing_by_regex(lang, w); // падает на больших словах((
 		const auto end = clock::now();
-		const double elapsed =
-			std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
-				.count()*1e-7;
-		// int time = (int) elapsed;
-		// int time = (end - start); // ((clock_t)100);
-		cout << " " << elapsed << endl;
-		// cout << is_belongs << endl;
-		times.push_back(elapsed);
+		const long long elapsed =
+			std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+				.count();
+		double time = (double)elapsed / 1000;
+		//cout << " " << time << endl;
+		times.push_back(time);
 		booleans.push_back(is_belongs);
-		if (elapsed >= 180) return;
+		if (time >= 180) return;
 	}
-	/*Logger::init_step("Test");
+	Logger::init_step("Test");
 	Logger::log(lang, r2, step, times, booleans);
-	Logger::finish_step();*/
+	Logger::finish_step();
 }
 
 void Tester::test(FiniteAutomaton lang, string r2, int step) {
@@ -48,17 +47,17 @@ void Tester::test(FiniteAutomaton lang, string r2, int step) {
 		// cout << w <<endl;
 		const auto start = clock::now();
 		bool is_belongs = lang.parsing_by_nfa(w);
+		// parsing_by_regex(lang, w); // падает на больших словах((
 		const auto end = clock::now();
-		cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
-		
-		const double elapsed =
-			std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
-				.count()*1e-6;
-		times.push_back(elapsed);
+		const long long elapsed =
+			std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+				.count();
+		double time = (double)elapsed / 1000;
+		//cout << " " << time << endl;
+		times.push_back(time);
 		booleans.push_back(is_belongs);
-		// int time = (end - start); // CLOCKS_PER_SEC;
-		cout << is_belongs << " " << elapsed << endl;
-		//words.push_back({i * step, time, is_belongs});
+		//cout << is_belongs << " " << elapsed << endl;
+		// words.push_back({i * step, time, is_belongs});
 		if (elapsed >= 180) return;
 	}
 
