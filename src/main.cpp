91930e1fd8ca4c09f7589addea38dce5bbb1a7b7
alina8031 <al_chibizova@mail.ru>
2022-10-29 16:40:49 +0300

    чистка мэина(#1)

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,5 @@
 using namespace std;
 
 int main() {
-	Example::tester();
 	cout << "Chipollino :-)\n";
-	Logger::init();
-	Logger::activate();
-	Example::tester();
-	Logger::deactivate();
-	Logger::finish();
 }
