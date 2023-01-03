d45b5cd2864aaa12b7903d43dfbc6969f3e236e7
Анна Терентьева <79749959+mathhyyn@users.noreply.github.com>
2023-01-03 19:10:53 +0300

    тайпчекер не удаляет повторения (De)Linearize / DeAnnote в посл-ти #209
    (задевает #169 )
    поправьте меня если что-то не так:
    * Annote - убираются дубли и если на вход подается DFA
    Annote.Annote == Annote
    Annote.Determinize == Determinize
    * Linearize - допускаются повторения и не убирается перед Глушковым
    Glushkov.Linearize.Linearize - без изменений
    * DeAnnote / DeLinearize - допускаются повторения

--- libs/Interpreter/src/Interpreter.cpp
+++ libs/Interpreter/src/Interpreter.cpp
@@ -476,6 +476,7 @@ optional<vector<Function>> Interpreter::build_function_sequence(
 							names_to_functions[predfunc][0].output ==
 								ObjectType::DFA) {
 							neededfuncs[i] = 0;
+							// удаление Annote перед DFA
 						}
 						if (predfunc == "Minimize" &&
 							(func == "Minimize" || func == "Determinize")) {
@@ -491,15 +492,17 @@ optional<vector<Function>> Interpreter::build_function_sequence(
 					}
 				} else {
 					if (predfunc == func) {
-						if (predfunc != "Reverse" && predfunc != "Complement") {
+						if (func != "Reverse" && func != "Complement" &&
+							func != "Linearize") {
 							neededfuncs[i - 1] = 0;
 						}
-					} else {
+					} /*else {
 						if (predfunc == "Linearize" &&
 							(func == "Glushkov" || func == "IlieYu")) {
 							neededfuncs[i - 1] = 0;
+							// удаление Linearize перед Glushkov
 						}
-					}
+					}*/
 				}
 			} else {
 				return nullopt;
@@ -526,7 +529,9 @@ optional<vector<Function>> Interpreter::build_function_sequence(
 					}
 				} else {
 					neededfuncs[i] = neededfuncs[i - 1];
-					neededfuncs[i - 1] = 0;
+					// neededfuncs[i - 1] = 0;
+					// удаление из последовательности повторений:
+					// DeLinearize.DeLinearize.DeAnnote == DeLinearize
 				}
 			} else if (names_to_functions[predfunc].size() == 2) {
 				if (names_to_functions[func][0].input == regex_type &&
