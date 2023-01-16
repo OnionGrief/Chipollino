2544bbd6be133ec10fee468abbee283a3437aed0
Rey <mrkill972@gmail.com>
2023-01-16 03:00:24 +0300

    добавление таблиц к автоматам #220

--- libs/AutomatonToImage/src/AutomatonToImage.cpp
+++ libs/AutomatonToImage/src/AutomatonToImage.cpp
@@ -16,5 +16,39 @@ void AutomatonToImage::to_image(string automat, int name) {
 	fo = fopen("./resources/input.dot", "wt");
 	fprintf(fo, "%s", automat.c_str());
 	fclose(fo);
-	system(cmd);
+	system(
+		"cd refal && refgo Preprocess+MathMode input.dot > error_refal0.txt");
+	system("cd refal && dot2tex -ftikz -tmath \"Mod_input.dot\" > input.tex");
+	system("cd refal && refgo Postprocess+MathMode input.tex > error_refal.txt "
+		   "2>&1");
+
+	system("cd refal && rm input.dot && rm input.tex");
+	
+	//автомат
+	ifstream infile_for_R("./refal/R_input.tex");
+	stringstream graph;
+	string s;
+	if (infile_for_R) {
+		for (; !infile_for_R.eof();) {
+			getline(infile_for_R, s);
+			graph << s << endl;
+		}
+	}
+	infile_for_R.close();
+
+
+	//таблица
+	ifstream infile_for_L("./refal/L_input.tex");
+	if (infile_for_L) {
+		for (; !infile_for_L.eof();) {
+			getline(infile_for_L, s);
+			graph << s << endl;
+		}
+	}
+	infile_for_L.close();
+	
+
+	
+
+	return graph.str();
 }
