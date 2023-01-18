1f0c39959e03f1f2cb05db78de3db84664fda64c
Анна Терентьева <79749959+mathhyyn@users.noreply.github.com>
2023-01-19 01:24:13 +0300

    фикс слияния #234

--- libs/Interpreter/src/Interpreter.cpp
+++ libs/Interpreter/src/Interpreter.cpp
@@ -232,7 +232,7 @@ optional<GeneralObject> Interpreter::apply_function(
 
 	log_template.set_parameter("name", func_id);
 	log_template.load_tex_template(func_id);
-	log_template.set_theory_flag(flags_values[Flags::theory]);
+	log_template.set_theory_flag(flags[Flag::weak_type_comparison]);
 
 	if (function.name == "Glushkov") {
 		return ObjectNFA(
