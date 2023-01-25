a36fa6de7b999bfbd75432db7f4eb53aee801733
TonitaN <a_nevod@mail.ru>
2023-01-25 08:44:29 +0300

    Пре-альфа авторазмещение

--- libs/AutomatonToImage/src/AutomatonToImage.cpp
+++ libs/AutomatonToImage/src/AutomatonToImage.cpp
@@ -51,9 +51,7 @@ string AutomatonToImage::to_image(string automat) {
 
 	if (!infile_for_L) return graph.str();
 
-	graph << "\\end{frame}" << endl << endl;
-	graph << "\\begin{frame}" << endl << endl;
-
+	// the table is adjusted for frames in the general renderer module
 	while (!infile_for_L.eof()) {
 		getline(infile_for_L, s);
 		graph << s << endl;
--- refal/compile.sh
+++ refal/compile.sh
@@ -1,3 +1,4 @@
 refc MathMode.ref
 refc Preprocess.ref
 refc Postprocess.ref
+refc FrameFormatter.ref
