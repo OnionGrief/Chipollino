94b6158e7b5011cdb6f472fe3c0a7ba74a0efd40
Анна Терентьева <annyshka20072002@gmail.com>
2023-01-27 02:44:23 +0300

    вернула файл со всеми ф/ями #236

--- apps/InterpreterApp/src/main.cpp
+++ apps/InterpreterApp/src/main.cpp
@@ -12,7 +12,8 @@ int main(int argc, char* argv[]) {
 	interpreter.set_log_mode(Interpreter::LogMode::all);
 
 	// Загружаем в интерпретатор файл с коммандами
-	string load_file = "./resources/all_functions.txt";
+	string load_file = "test.txt";
+	//string load_file = "./resources/all_functions.txt";
 	if (argc > 1) load_file = argv[1];
 	if (interpreter.run_file(load_file)) {
 		interpreter.generate_log("./resources/report.tex");
