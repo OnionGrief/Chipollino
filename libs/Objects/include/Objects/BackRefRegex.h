c7219a5e4e4759c01da794f64d0e3481ff95fd04
Alexander Delman <adelman2112@gmail.com>
2023-11-07 01:41:57 +0300

    (#271) bregex tree building

--- libs/Objects/include/Objects/BackRefRegex.h
+++ libs/Objects/include/Objects/BackRefRegex.h
@@ -4,10 +4,28 @@ using namespace std;
 
 class BackRefRegex : public AlgExpression {
   private:
+	int cell_number = 0;
+
+	void copy(const AlgExpression*) override;
 	// возвращает указатель на new BackRefRegex
-	AlgExpression* make() const override;
+	BackRefRegex* make() const override;
+
+	string type_to_str() const override;
+
+	BackRefRegex* expr(const vector<Lexeme>&, int, int) override;
+	BackRefRegex* scan_ref(const vector<Lexeme>&, int, int);
+	BackRefRegex* scan_square_br(const vector<Lexeme>&, int, int);
 
   public:
+	BackRefRegex() = default;
+	BackRefRegex(const string&);
+
+	BackRefRegex* make_copy() const override;
+	BackRefRegex(const BackRefRegex&);
+
 	// dynamic_cast к типу BackRefRegex*
 	template <typename T> static BackRefRegex* cast(T* ptr);
+	template <typename T> static const BackRefRegex* cast(const T* ptr);
+
+	string to_txt() const override;
 };
