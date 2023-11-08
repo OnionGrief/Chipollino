98ee9f8b11f48e47164397e2b4d994d4492f8fd9
DanilaKn <dak151449@gmail.com>
2023-11-08 17:05:05 +0300

    (#283) Разделил тесты по типам

--- apps/UnitTestsApp/src/main.cpp
+++ apps/UnitTestsApp/src/main.cpp
@@ -1,11 +1,11 @@
-#include "TestsApp/Example.h"
+#include "UnitTestsApp/Example.h"
 #include "gtest/gtest.h"
 #include <iostream>
 #include <thread>
 using namespace std;
 
 int main(int argc, char** argv) {
-	cout << "Test\n";
+	cout << "Unit Tests\n";
 	// Тестирование
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
