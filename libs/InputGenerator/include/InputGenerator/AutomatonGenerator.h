a1cb678b1df4a493a0bee51fd05fc04956d3380a
Robby-the-paladin <mea250802@gmail.com>
2024-02-24 19:42:04 +0300

    (#318) generator via attribute grammar implemented

--- libs/InputGenerator/include/InputGenerator/AutomatonGenerator.h
+++ libs/InputGenerator/include/InputGenerator/AutomatonGenerator.h
@@ -9,19 +9,14 @@
 #include <set>
 #include <utility>
 #include <iostream>
+#include <set>
+#include "AutomataParser/Lexer.h"
+#include "AutomataParser/Parser.h"
 
 enum class FA_type { MFA, FA, DFA };
 
 class AutomatonGenerator {
 private:
-    struct MFA_edge {
-        int ref;
-        int to;
-
-        std::set<int> open;
-        std::set<int> close;
-    };
-
     struct AutomatonGeneratorConstants {
         static inline int terminal_probability = 20;
         static inline int max_memory_cells_number = 10;
@@ -34,36 +29,157 @@ private:
         // вероятность генерации закрытия для ячейки, при генерации взаимодействия
         static inline int action_closing_probability = 50;
         static inline bool initial_state_not_terminal = false;
-    }; // namespace AutomatonGeneratorConstants
+    };
+
+    struct FAtransition {
+        int end;
+
+        Symbol symbol;
+
+        // MFA fields
+        std::unordered_set<int> close;
+        std::unordered_set<int> open;
+
+        // PDA fields
+        Symbol pop;
+        std::vector<Symbol> push;
+    };
+
+    struct StateDescription {
+        int index;
+
+        bool terminal;
+        bool initial;
+    };
 
     int seed_it, memory_cells_number;
     std::vector<char> alphabet;
-    // Ячейки, которые могут быть открыты при попадании в состояние
-    std::map<int, std::set<int>> open_cells;
-    // Переходы в MFA, необходимы для проверки корректности MFA переходов
-    std::map<int, std::vector<MFA_edge>> MFA_graph;
-    // Существование перехода по символу из состояния (для DFA)
-    std::map<std::pair<int, std::string>, bool> symbol_transitions;
-
-    void calculate_open_cells(int state, std::map<int, std::set<int>>& _open_cells); // NOLINT(runtime/references)
-    bool is_MFA_transition_legal(int state, MFA_edge edge);
-    void add_MFA_transition(int state, MFA_edge edge);
-
-    bool is_DFA_trasition_legal(int state, std::string symbol);
-    void add_DFA_transition(int state, std::string symbol);
 
     void change_seed();
 
 	// возвращает true с вероятностью percentage%
 	bool dice_throwing(int percentage);
 
+    Symbol generate_symbol() {}
+
+    std::vector<StateDescription> stateDescriptions;
+    std::vector<std::vector<FAtransition>> graph;
+
+    void generate_graph();
+
     std::stringstream output;
+
+    std::queue<std::string> TERMINAL, STRING, NUMBER, LETTER, DIGIT, STACK_SYMBOLS;
+    int cur_state = 0, cur_transition = 0;
+
+    std::map<std::string, lexy_ascii_child::children_range::iterator> rewriting_rules;
+    std::set<std::string> attributes;
+
+    std::map<std::string, std::function<bool()>> parse_func = {
+        // transition --> stmt (MFA? memory_lists : EPS) (PDA? stack_actions : EPS);
+        {"states", [=](){
+            for (int i = 0; i < stateDescriptions.size(); i++) {
+                if (stateDescriptions[i].initial || stateDescriptions[i].terminal) {
+                    STRING.push(std::to_string(stateDescriptions[i].index));
+                    if (stateDescriptions[i].initial)
+                        TERMINAL.push("initial_state");
+                    if (stateDescriptions[i].terminal)
+                        TERMINAL.push("terminal");
+                    TERMINAL.push(";");
+                }
+            }
+            TERMINAL.push("...");
+            auto transition = rewriting_rules["states"];
+            auto res = parse_alternative(*transition);
+            if (res) {
+                std::cout << "\n";
+            }
+            return res;
+        }},
+        {"state", [=](){
+            auto transition = rewriting_rules["state"];
+            auto res = parse_alternative(*transition);
+            if (res) {
+                std::cout << "\n";
+            }
+            return res;
+        }},
+        {"transition", [=](){
+            while(cur_state < graph.size() && cur_transition >= graph[cur_state].size()) {
+                cur_state++;
+                cur_transition = 0;
+            }
+            if (cur_state < graph.size()) {
+                // stmt
+                STRING.push(std::to_string(cur_state));
+                STRING.push(std::to_string(graph[cur_state][cur_transition].end));
+                if (graph[cur_state][cur_transition].symbol.is_epsilon()) {
+                    TERMINAL.push("eps");
+                } else {
+                    if (graph[cur_state][cur_transition].symbol.is_ref()) {
+                        TERMINAL.push("&");
+                        NUMBER.push(std::to_string(graph[cur_state][cur_transition].symbol.get_ref()));
+                    } else {
+                        LETTER.push(graph[cur_state][cur_transition].symbol); 
+                    }
+                }
+
+                // memory_lists
+                for (auto cell : graph[cur_state][cur_transition].open) {
+                    NUMBER.push(std::to_string(cell));
+                    TERMINAL.push("o");
+                }
+                for (auto cell : graph[cur_state][cur_transition].close) {
+                    NUMBER.push(std::to_string(cell));
+                    TERMINAL.push("c");
+                }
+
+                // stack_actions
+                STACK_SYMBOLS.push(graph[cur_state][cur_transition].pop);
+                for (auto sym : graph[cur_state][cur_transition].push)
+                    STACK_SYMBOLS.push(sym);
+
+                TERMINAL.push(";");
+
+                cur_transition++;
+            }
+
+            auto transition = rewriting_rules["transition"];
+            auto res = parse_alternative(*transition);
+            if (res)
+                output << "\n";
+            return res;
+        }},
+    };
+
+    // Поиск рекурсивный поиск вершин с названиями из names, игнорируя спуск в вершины из exclude
+	static std::vector<lexy_ascii_child> find_children(
+		lexy_ascii_tree& tree, // NOLINT(runtime/references)
+		const std::set<std::string>& names, const std::set<std::string>& exclude = {});
+
+	// лексема первого потомка для вершины lexy
+	static std::string first_child(lexy_ascii_child::children_range::iterator it);
+
+	// лексема первого потомка для вершины lexy
+	static std::string first_child(lexy_ascii_child it);
 
     void generate_alphabet(int max_alphabet_size);
 
-    void generate_states_description(int states_number);
+    void read_symbols(int num);
 
-    void generate_transitions(int transitions_number, int states_number, FA_type type);
+    void parse_attribute(lexy_ascii_child ref);
+
+    bool parse_transition(std::string name);
+
+    bool parse_nonterminal(lexy_ascii_child ref);
+    
+    bool parse_reserved(std::string res_case);
+
+    bool parse_terminal(lexy_ascii_child ref);
+
+    bool parse_alternative(lexy_ascii_child ref);
+
+    void grammar_parser(std::string grammar_file, lexy_ascii_tree& tree); // NOLINT(runtime/references)
 
 public:
     explicit AutomatonGenerator(FA_type type = FA_type::FA);
--- libs/InputGenerator/src/AutomatonGenerator.cpp
+++ libs/InputGenerator/src/AutomatonGenerator.cpp
@@ -4,8 +4,8 @@ using std::ofstream;
 using std::string;
 using std::to_string;
 using std::rand;
-
-
+using std::vector;
+using std::set;
 
 void AutomatonGenerator::change_seed() {
 	seed_it++;
@@ -21,186 +21,35 @@ bool AutomatonGenerator::dice_throwing(int percentage) {
     return false;
 }
 
-void AutomatonGenerator::calculate_open_cells(int state, std::map<int, std::set<int>>& _open_cells) {
-    std::set<int> states_to_check;
-    states_to_check.insert(state);
-    
-    while (!states_to_check.empty()) {
-        int cur_state = *states_to_check.begin();
-        for (auto transition : MFA_graph[cur_state]) {
-            for (auto cell : _open_cells[cur_state]) {
-                // Если в текущем состоянии ячейка открыта для записи и переход её не закрывает
-                bool pass = !transition.close.count(cell);
-                // Если раньше в open_cells для состояния transition.to ячейка была закрыта, то это состояние надо проверить
-                if (!_open_cells[transition.to].count(cell) && pass) {
-                    _open_cells[transition.to].insert(cell);
-                    states_to_check.insert(transition.to);
-                }
-            }
-            // Для всех ячеек, открываемых переходом
-            for (auto cell : transition.open) {
-                // Если раньше в open_cells для состояния transition.to ячейка была закрыта, то это состояние надо проверить
-                if (!_open_cells[transition.to].count(cell)) {
-                    _open_cells[transition.to].insert(cell);
-                    states_to_check.insert(transition.to);
-                }
-            }
-        }
-    }
-}
+void AutomatonGenerator::generate_graph() {
+    int initial = 0, states_number = 10;
+    int max_edges_number = states_number * (states_number - 1) / 2 + 3;
+    int edges_number = rand() % (max_edges_number - states_number + 1) + states_number - 1;
 
-bool AutomatonGenerator::is_MFA_transition_legal(int _state, MFA_edge edge) {
-    // Чтение из открывающейся для записи ячейки
-    if (edge.open.count(edge.ref))
-        return false;
+    graph.resize(states_number);
 
-    auto _open_cells = open_cells;
+    std::vector<int> included_states, excluded_states;
 
-    MFA_graph[_state].push_back(edge);
-
-    calculate_open_cells(_state, _open_cells);
-
-    for (auto state : _open_cells) {
-        for (auto transition : MFA_graph[state.first]) {
-            for (auto cell : state.second) {
-                // Ячейка может быть открыта и она открывается повторно или по ней существует переход
-                if (transition.open.count(cell) || transition.ref == cell) {
-                    MFA_graph[_state].pop_back();
-                    return false;
-                }
-            }
-        }
+    included_states.push_back(initial);
+    for (int i = 1; i < states_number; i++) {
+        excluded_states.push_back(i);
     }
 
-    MFA_graph[_state].pop_back();
-    return true;    
-}
-
-void AutomatonGenerator::add_MFA_transition(int state, MFA_edge edge) {
-    MFA_graph[state].push_back(edge);
-
-    calculate_open_cells(state, open_cells);
-}
-
-bool AutomatonGenerator::is_DFA_trasition_legal(int state, string symbol) {
-    return !symbol_transitions[{state, symbol}];
-}
-
-void AutomatonGenerator::add_DFA_transition(int state, string symbol) {
-    symbol_transitions[{state, symbol}] = true;
-}
-
-void AutomatonGenerator::generate_transitions(int transitions_number, int states_number, FA_type type) {
-    bool is_print_transit = false;
-
-    for (int i = 0; i < transitions_number; i++) {
-        // std::cout << "- " << i << " " << std::endl;
-        MFA_edge new_edge;
-
-        change_seed();
-        int beg = rand() % states_number;
-
-        change_seed();
-        int end = rand() % states_number;
-
-        new_edge.to = end;
-
-        change_seed();
-        string transition_sym;
-        bool generation_success = true;
-        int tries = 0;
-        do {
-            generation_success = true;
-            transition_sym = "";
-            int transition_symbol_num = rand() % (alphabet.size() + memory_cells_number);
-            if (transition_symbol_num < alphabet.size()) {
-                transition_sym.push_back(alphabet[transition_symbol_num]);
-                new_edge.ref = -1;
-                if (type == FA_type::DFA) {
-                    generation_success = is_DFA_trasition_legal(beg, transition_sym);
-                }
-            } else {
-                transition_sym = "&" + to_string(transition_symbol_num - alphabet.size());
-                new_edge.ref = transition_symbol_num - alphabet.size();
-            }
-            if (type == FA_type::MFA)
-                generation_success = is_MFA_transition_legal(beg, new_edge);
-
-            tries++;
-            // std::cout << "tries: " << tries << std::endl;
-        } while (!generation_success && tries < 10);
-
-
-        if (!generation_success)
-            continue;
-
-        if (is_print_transit) {
-            output << ";\n";
+    for (int i = 0; i < edges_number; i++) {
+        FAtransition cur;
+        int beg = included_states[rand() % included_states.size()];
+        cur.end = included_states[rand() % included_states.size()];
+        if (!excluded_states.empty()) {
+            int ind = rand() % excluded_states.size();
+            cur.end = excluded_states[ind];
+            included_states.push_back(excluded_states[ind]);
+            excluded_states.erase(excluded_states.begin() + ind);
         }
-        output << to_string(beg) << " " << to_string(end) << " " << transition_sym;
-        is_print_transit = true;
-        
-        if (type == FA_type::MFA) {
-			for (int j = 0; j < memory_cells_number; j++) {
-				if (dice_throwing(AutomatonGeneratorConstants::action_probability)) {
-					if (dice_throwing(AutomatonGeneratorConstants::action_closing_probability)) {
-                        new_edge.close.insert(j);
-                        output << " " << j << " c";
-                    } else {
-                        new_edge.open.insert(j);
-                        if (is_MFA_transition_legal(beg, new_edge)) {
-                            output << " " << j << " o";
-                        } else {
-                            new_edge.open.erase(j);
-                        }
-                    }
-                }
-            }
-        }
+        cur.symbol = generate_symbol();
+        cur.pop = "$";
 
-        // if (i + 1 != transitions_number)
-        //     output << ";\n";
-        // else
-        //     output << "\n";
-
-        if (FA_type::MFA == type) {
-            add_MFA_transition(beg, new_edge);
-        }
-        if (FA_type::DFA == type) {
-            add_DFA_transition(beg, transition_sym);
-        }
-        
+        graph[beg].push_back(cur);
     }
-    output << "\n";
-    // std::cout << "END generate_transitions\n";
-}
-
-void AutomatonGenerator::generate_states_description(int states_number) {
-    change_seed();
-    int initial = rand() % states_number;
-
-    std::set<int> terminal_states;
-
-    for (int i = 0; i < states_number; i++) {
-        if (initial == i) {
-            output << to_string(i) << " initial_state";
-
-            change_seed();
-            if (AutomatonGeneratorConstants::initial_state_not_terminal && dice_throwing(AutomatonGeneratorConstants::terminal_probability)) {
-                output << " terminal";
-            }
-        } else {
-            if (dice_throwing(AutomatonGeneratorConstants::terminal_probability)) {
-                terminal_states.insert(i);
-            }
-        }
-    }
-
-    for (auto state : terminal_states) {
-        output << ";\n" << to_string(state) << " terminal";
-    }
-
-    output << "\n...\n";
 }
 
 void AutomatonGenerator::generate_alphabet(int max_alphabet_size) {
@@ -219,39 +68,7 @@ void AutomatonGenerator::generate_alphabet(int max_alphabet_size) {
 }
 
 AutomatonGenerator::AutomatonGenerator(FA_type type) {
-    if (type == FA_type::FA || type == FA_type::DFA) {
-        output << "FA {\n";
-        memory_cells_number = 0;
-    } else {
-        output << "MFA {\n";
-
-        change_seed();
-        memory_cells_number = rand() % AutomatonGeneratorConstants::max_memory_cells_number + 1;
-    }
-
-    change_seed();
-    int states_number = rand() % AutomatonGeneratorConstants::max_states_number + 1;
-
-    generate_states_description(states_number);
-
-    generate_alphabet(AutomatonGeneratorConstants::alphabet_size);
-
-    change_seed();
-    int transitions_number = rand() % (states_number * (states_number - 1) / 2 +
-     AutomatonGeneratorConstants::additional_max_transitions_number) + 1;
-
-    generate_transitions(transitions_number, states_number, type);
-
-    output << "}";
-    // std::cout << output.str() << std::endl;
-
-    // std::cout << output.str() << std::endl;
-
-    // for (auto elem: alphabet) {
-    //     string s = "";
-    //     std::cout << s + elem + " | ";
-    // }
-    // std::cout << ". " << alphabet.size() << " " <<  memory_cells_number << std::endl;
+    
 }
 
 void AutomatonGenerator::write_to_file(string filename) {
@@ -269,4 +86,177 @@ void AutomatonGenerator::set_terminal_probability(int elem) {
 void AutomatonGenerator::set_initial_state_not_terminal(bool f)
 {
     AutomatonGeneratorConstants::initial_state_not_terminal = f;
+}
+
+vector<lexy_ascii_child> AutomatonGenerator::find_children(lexy_ascii_tree& tree, const set<string>& names,
+											   const set<string>& exclude) {
+	vector<lexy_ascii_child> result;
+	int skip = 0;
+	for (auto [event, node] : tree.traverse()) {
+		auto depth = 0;
+		switch (event) {
+		case lexy::traverse_event::enter:
+			if (!skip && names.count(node.kind().name())) {
+				result.push_back(node);
+			}
+			if (exclude.count(node.kind().name())) {
+				skip++;
+			}
+			depth++;
+			break;
+		case lexy::traverse_event::exit:
+			if (exclude.count(node.kind().name())) {
+				skip--;
+			}
+			depth--;
+			break;
+
+		case lexy::traverse_event::leaf:
+			if (!skip && names.count(node.kind().name())) {
+				result.push_back(node);
+			}
+			break;
+		}
+	}
+	return result;
+}
+
+string Parser::first_child(lexy::_pt_node<lexy::_bra, void>::children_range::iterator it) {
+	return lexy::as_string<string, lexy::ascii_encoding>(it->children().begin()->token().lexeme());
+}
+
+string Parser::first_child(lexy::_pt_node<lexy::_bra, void> it) {
+	return lexy::as_string<string, lexy::ascii_encoding>(it.children().begin()->token().lexeme());
+}
+
+bool AutomatonGenerator::parse_reserved(std::string res_case) {
+    // std::cout << "parse_reserved: " << res_case << "\n";
+    if (res_case == "EPS")
+        return true;
+
+    if (res_case == "LETTER") {
+        if (!LETTER.empty()) {
+            output << " " << LETTER.front();
+            return true;
+        }
+        return false;
+    }
+    if (res_case == "DIGIT") {
+        if (!DIGIT.empty()) {
+            output << " " << LETTER.front();
+            return true;
+        }
+        return false;
+    }
+    if (res_case == "STRING") {
+        if (!STRING.empty()) {
+            output << " " << LETTER.front();
+            return true;
+        }
+        return false;
+    }
+    if (res_case == "NUMBER") {
+        if (!NUMBER.empty()) {
+            output << " " << LETTER.front();
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
+
+bool AutomatonGenerator::parse_nonterminal(lexy::_pt_node<lexy::_bra, void> ref) {
+    return parse_transition(first_child(ref));
+}
+
+bool AutomatonGenerator::parse_terminal(lexy::_pt_node<lexy::_bra, void> ref) {
+    // std::cout << "parsing terminal\n";
+    auto it = ref.children().begin();
+    it++;
+    std::string to_read = lexy::as_string<string, lexy::ascii_encoding>(it->token().lexeme());
+    // std::cout << "\n" << to_read << " " << cur_pos << " " << file.size() << "\n";
+    // std::cout << file.substr(cur_pos, to_read.size()) << "\n";
+    auto res = (!TERMINAL.empty() && TERMINAL.front() == to_read);
+    if (res) {
+        output << TERMINAL.front();
+        TERMINAL.pop();
+    }
+    // std::cout << "terminal? --> " << res << "\n";
+    return res;
+}
+
+void AutomatonGenerator::parse_attribute(lexy::_pt_node<lexy::_bra, void> ref) {
+    // std::cout << "\nparsing attribute\n";
+    auto it = ref.children().begin();
+    while(std::string(it->kind().name()) != "nonterminal")
+        it++;
+    attributes.insert(first_child(it));
+}
+
+bool AutomatonGenerator::parse_alternative(lexy::_pt_node<lexy::_bra, void> ref) {
+    // std::cout << "Enter alternative\n";
+    bool read = true;
+    for (auto it = ref.children().begin(); it != ref.children().end(); it++) {
+        // std::cout << "tring to parse: " << it->kind().name() << "\n";
+        if (it->kind().is_token() && lexy::as_string<string, lexy::ascii_encoding>(it->token().lexeme()) == "|") {
+            if (read)
+                return true;
+            read = true;
+            continue;
+        }
+        if (!read) {
+            // std::cout << "Oops... read failed\n";
+            continue;
+        }
+        if (it->kind().is_token() && lexy::as_string<string, lexy::ascii_encoding>(it->token().lexeme()) == "(") {
+            it++;
+            if (attributes.count(first_child(it->children().begin()))) {
+                while (it->kind().is_token() || std::string(it->kind().name()) != "alternative")
+                    it++;
+                parse_alternative(*it);
+            } else {
+                while (!it->kind().is_token() || lexy::as_string<string, lexy::ascii_encoding>(it->token().lexeme()) != ":")
+                    it++;
+
+                while (it->kind().is_token() || std::string(it->kind().name()) != "alternative")
+                    it++;
+                parse_alternative(*it);
+            }
+            while (it->kind().is_token() && lexy::as_string<string, lexy::ascii_encoding>(it->token().lexeme()) != ")")
+                it++;
+        }
+        // std::cout << "terminal?\n";
+        if (std::string(it->kind().name()) == "terminal") {
+            read &= parse_terminal(*it);
+            continue;
+        }
+        if (std::string(it->kind().name()) == "nonterminal") {
+            read &= parse_nonterminal(*it);
+            continue;
+        }
+        if (std::string(it->kind().name()) == "reserved") {
+            parse_reserved(first_child(it));
+            continue;
+        }
+        if (std::string(it->kind().name()) == "attribute") {
+            parse_attribute(*it);
+            continue;
+        }
+    }
+    return read;
+}
+
+bool AutomatonGenerator::parse_transition(std::string name) {
+    // std::cout << name << "\n";
+
+    if (!rewriting_rules.count(name)) {
+        return false;
+    }
+
+    if (!parse_func.count(name)) {
+        auto transition = rewriting_rules[name];
+        return parse_alternative(*transition);  
+    }
+
+    return parse_func[name]();
 }
